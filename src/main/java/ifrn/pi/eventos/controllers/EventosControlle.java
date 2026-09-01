dc6c0e7d8302a9867ef890372a3755dfb5a0d33b
Anderson <anderson.fernandes1@escolar.ifrn.edu.br>
2026-09-01 13:38:18 -0300

    Enviando dados do fórmulario para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosControlle.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosControlle.java
@@ -1,7 +1,11 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller	
 public class EventosControlle {
@@ -11,4 +15,11 @@ public class EventosControlle {
 		return "formEvento";
 	}
 	
+	@PostMapping("/eventos") 
+	public String adicionar (Evento evento) {
+		
+		System.out.println(evento);
+		
+		return "evento-adicionado";
+	}
 }
